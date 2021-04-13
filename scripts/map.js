1ee06ec32b06f53083ff21524977e0fdc4ecee2c
Robert M Newton <robert.m.newton7@gmail.com>
2021-04-13 13:27:57 +1000

    Add fences, bushes and trees. (new collision rules needed)

--- scripts/map.js
+++ scripts/map.js
@@ -2,18 +2,17 @@ var greenBackground = new background("green");
 var orangeBackground = new background("orange");
 
 var papaSonia = new nonPlayerCharacter(240, 240, 20, 40, "skyblue");
-var Jona = new nonPlayerCharacter(500, 30, 40, 70, "brown");
 
 var map = [
-    [greenBackground, papaSonia, createRock(40, 50, 10, 5, "grey"), createRock(120, 152, 10, 5, "grey")], //C1 R1
+    [greenBackground, papaSonia, newRock(40, 50, 10, 5, "grey"), newTree(442, 149),newBush(550, 240, 50, 50), newFence(547, 125, "horizontal")], //C1 R1
     [greenBackground], //C2 R1
-    [greenBackground, Jona], //C3 R1
+    [greenBackground], //C3 R1
     [orangeBackground], //C4 R1
     [orangeBackground], //C5 R1
     [greenBackground], //C1 R2
     [greenBackground], //C2 R2
     [greenBackground], //C3 R2
-    [orangeBackground], //C4 R2
+    [orangeBackground], //C4 R2ddd
     [orangeBackground], //C5 R2
     [greenBackground], //C1 R3
     [greenBackground], //C2 R3
--- scripts/objects.js
+++ scripts/objects.js
@@ -52,7 +52,7 @@ function rock(x, y, width, height, colour)  {
         var xAverage = (this.collisionBox.x1 + this.collisionBox.x2 + myPlayer.collisionBox.x1 + myPlayer.collisionBox.x2) / 4;
         var yAverage = (this.collisionBox.y1 + this.collisionBox.y2 + myPlayer.collisionBox.y1 + myPlayer.collisionBox.y2) / 4;
 
-        if (xAverage <= this.collisionBox.x2 && xAverage >= this.collisionBox.x1 && yAverage >= this.collisionBox.y2 && yAverage <= this.collisionBox.y1) {
+        if (xAverage < this.collisionBox.x2 && xAverage > this.collisionBox.x1 && yAverage > this.collisionBox.y1 && yAverage < this.collisionBox.y2) {
             return true;
         }
 
@@ -68,9 +68,155 @@ function bush(x, y, width, height) {
         width: width,
         height: height
     };
+    this.collisionBox = {
+        x1: this.position.x,
+        x2: this.position.x + this.dimensions.width,
+        y1: this.position.y + this.dimensions.height - 20,
+        y2: this.position.y + this.dimensions.height
+    };
     this.speech = ["A bush..."]
+
+    this.update = function() {
+        context.fillStyle = "darkgreen";
+        context.fillRect(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
+    }
+
+    this.collision = function() {
+        var xAverage = (this.collisionBox.x1 + this.collisionBox.x2 + myPlayer.collisionBox.x1 + myPlayer.collisionBox.x2) / 4;
+        var yAverage = (this.collisionBox.y1 + this.collisionBox.y2 + myPlayer.collisionBox.y1 + myPlayer.collisionBox.y2) / 4;
+
+        if (xAverage < this.collisionBox.x2 && xAverage > this.collisionBox.x1 && yAverage > this.collisionBox.y1 && yAverage < this.collisionBox.y2) {
+            return true;
+        }
+
+        return false;
+    }
 }
 
-function createRock(x, y, width, height, colour) {
+function horizontalFence(x, y) {
+    this.position = {
+        x: x,
+        y: y
+    };
+    this.dimensions = {
+        width: 70,
+        height: 40
+    };
+    this.collisionBox = {
+        x1: this.position.x,
+        x2: this.position.x + this.dimensions.width,
+        y1: this.position.y + this.dimensions.height - 10,
+        y2: this.position.y + this.dimensions.height
+    };
+    this.speech = ["A fence..."]
+
+    this.update = function() {
+        context.fillStyle = "brown";
+        context.fillRect(this.position.x, this.position.y + 10, this.dimensions.width, 10);
+        context.fillRect(this.position.x, this.position.y + 25, this.dimensions.width, 10);
+        context.fillRect(this.position.x, this.position.y, 5, this.dimensions.height);
+        context.fillRect(this.position.x + this.dimensions.width, this.position.y, -5, this.dimensions.height);
+    }
+
+    this.collision = function() {
+        var xAverage = (this.collisionBox.x1 + this.collisionBox.x2 + myPlayer.collisionBox.x1 + myPlayer.collisionBox.x2) / 4;
+        var yAverage = (this.collisionBox.y1 + this.collisionBox.y2 + myPlayer.collisionBox.y1 + myPlayer.collisionBox.y2) / 4;
+
+        if (xAverage < this.collisionBox.x2 && xAverage > this.collisionBox.x1 && yAverage > this.collisionBox.y1 && yAverage < this.collisionBox.y2) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+function verticalFence(x, y) {
+    this.position = {
+        x: x,
+        y: y
+    };
+    this.dimensions = {
+        width: 5,
+        height: 70
+    };
+    this.collisionBox = {
+        x1: this.position.x,
+        x2: this.position.x + this.dimensions.width,
+        y1: this.position.y,
+        y2: this.position.y + this.dimensions.height
+    };
+    this.speech = ["A fence..."]
+
+    this.update = function() {
+        context.fillStyle = "brown";
+        context.fillRect(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
+    }
+
+    this.collision = function() {
+        var xAverage = (this.collisionBox.x1 + this.collisionBox.x2 + myPlayer.collisionBox.x1 + myPlayer.collisionBox.x2) / 4;
+        var yAverage = (this.collisionBox.y1 + this.collisionBox.y2 + myPlayer.collisionBox.y1 + myPlayer.collisionBox.y2) / 4;
+
+        if (xAverage < this.collisionBox.x2 && xAverage > this.collisionBox.x1 && yAverage > this.collisionBox.y1 && yAverage < this.collisionBox.y2) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+function tree(x, y) {
+    this.position = {
+        x: x,
+        y: y
+    };
+    this.dimensions = {
+        width: 20,
+        height: 100
+    };
+    this.collisionBox = {
+        x1: this.position.x,
+        x2: this.position.x + this.dimensions.width,
+        y1: this.position.y + this.dimensions.height - 10,
+        y2: this.position.y + this.dimensions.height
+    };
+    this.speech = ["A tree..."]
+
+    this.update = function() {
+        context.fillStyle = "brown";
+        context.fillRect(this.position.x, this.position.y, this.dimensions.width, this.dimensions.height);
+
+        context.fillStyle = "darkgreen";
+        context.fillRect(this.position.x - 30, this.position.y - 60, 80, 60);
+    }
+
+    this.collision = function() {
+        var xAverage = (this.collisionBox.x1 + this.collisionBox.x2 + myPlayer.collisionBox.x1 + myPlayer.collisionBox.x2) / 4;
+        var yAverage = (this.collisionBox.y1 + this.collisionBox.y2 + myPlayer.collisionBox.y1 + myPlayer.collisionBox.y2) / 4;
+
+        if (xAverage < this.collisionBox.x2 && xAverage > this.collisionBox.x1 && yAverage > this.collisionBox.y1 && yAverage < this.collisionBox.y2) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+function newRock(x, y, width, height, colour) {
     return new rock(x, y, width, height, colour);
+}
+
+function newBush(x, y, width, height) {
+    return new bush(x, y, width, height);
+}
+
+function newFence(x, y, type) {
+    if (type == "horizontal") {
+        return new horizontalFence(x, y);
+    } else if (type == "vertical") {
+        return new verticalFence(x, y);
+    }
+}
+
+function newTree(x, y) {
+    return new tree(x, y);
 }
